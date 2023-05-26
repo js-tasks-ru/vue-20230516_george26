d23c035d1765bf9c1f28648799d92af9b7d1c69d
georgegla <zhoric9@gmail.com>
2023-05-26 15:15:38 +0300

    solved the tasks of the first section

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -15,3 +15,30 @@ function fetchMeetupById(meetupId) {
 }
 
 // Требуется создать Vue приложение
+
+const App = defineComponent({
+  name: 'App',
+
+  data() {
+    return {
+    value: '',
+    meetupTitle: null,
+    }
+  },
+
+  watch: {
+    value: {
+      handler() {
+        fetchMeetupById(this.value).then(( { title } ) => this.meetupTitle = title)
+      }
+    },
+    immediate: true,
+  }
+})
+
+
+const app = createApp(App);
+
+const vm = app.mount('#app');
+
+
